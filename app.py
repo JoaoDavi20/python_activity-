35c8d0a63f4275b4cd5fb1d94cd9a2b74c50ba6c
JoaoDavi20 <joaodavi@usf.edu>
2025-11-23 22:00:18 -0500

    Add calculation and conversion

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 money = input("How much money you make per hour?")
 hour_of_work =input("How much hours you worked this week?")
+
+money = float(money)
+hour_of_work = int(hour_of_work)
+
+weekly_payement = money * hour_of_work
