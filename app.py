cadedf2db607f91d9a2f660abd2e46415ecb3842
JoaoDavi20 <joaodavi@usf.edu>
2025-11-23 22:09:19 -0500

    Add basic error handling for use input

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 money = input("How much money you make per hour?")
 hour_of_work =input("How much hours you worked this week?")
 
-money = float(money)
-hour_of_work = int(hour_of_work)
+try:
+    money = float(money)
+except ValueError:
+    print("Please put a valid number.")
+    exit()
+
+
+try:
+    hour_of_work = int(hour_of_work)
+except ValueError:
+    print("Please put a valid number.")
+    exit()
 
 weekly_payement = money * hour_of_work
 
 print(f"Your payment this week will be: ${weekly_payement}")
+
